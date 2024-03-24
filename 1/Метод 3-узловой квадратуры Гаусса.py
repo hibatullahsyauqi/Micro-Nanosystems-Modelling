dd344364b488dd37dc9757de04c91b737fcbf39e
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-03-24 13:52:35 +0300

    corrected formula

--- 1/Метод 3-узловой квадратуры Гаусса.py
+++ 1/Метод 3-узловой квадратуры Гаусса.py
@@ -1,19 +1,19 @@
 import math as math
-N = 10
+N = 100
 a = 0
 b = 1
 real = math.exp(1) - 1
 
 def f(x):
     return math.exp(x)
-for j in range (9):
+for j in range (99):
     sum = 0.0
     h = (b-a)/N
     for i in range (N):
-        ya = 5*f(-(h/2)*math.sqrt(3/5))
-        yb = 8*f(0)
-        yc = 5*f((h/2)*math.sqrt(3/5))
-        sum += (ya+yb+yc)*(h/18)
+        ya = (5/9)*f(((h*i/2)*(-math.sqrt(3)/math.sqrt(5)))+((b+a)/2))
+        yb = (8/9)*f(((h*i/2)*0)+((b+a)/2))
+        yc = (5/9)*f(((h*i/2)*(math.sqrt(3)/math.sqrt(5)))+((b+a)/2))
+        sum += (ya+yb+yc)*h/2
         error = real - sum
     print(f'integral: ', sum, '\th: ', h, '\terror: ', error)
     N *= 10
