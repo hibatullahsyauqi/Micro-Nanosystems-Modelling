b5bdde6d39a2bdbc46030aa2786be79242813e09
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-03-30 12:59:24 +0300

    LUdecomp file

---
+++ 6. Методы LU/LUdecomp.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def LUdecomp(a):
+    """
+    Выполняет LU-разложение с использованием метода Дулиттла.
+
+    Параметры:
+        a: numpy.ndarray
+            Матрица коэффициентов системы уравнений.
+
+    Возвращает:
+        a: numpy.ndarray
+            Объединенная нижняя и верхняя треугольные матрицы [L|U].
+    """
+    n = len(a)
+    for k in range(0, n - 1):
+        for i in range(k + 1, n):
+            if a[i, k] != 0.0:
+                lam = a[i, k] / a[k, k]
+                a[i, k + 1:n] = a[i, k + 1:n] - lam * a[k, k + 1:n]
+                a[i, k] = lam
+    return a
+
+def LUsolve(a, b):
+    """
+    Решает систему уравнений с помощью прямой и обратной подстановки.
+
+    Параметры:
+        a: numpy.ndarray
+            Объединенная нижняя и верхняя треугольные матрицы [L|U].
+        b: numpy.ndarray
+            Вектор констант.
+
+    Возвращает:
+        x: numpy.ndarray
+            Вектор решений.
+    """
+    n = len(a)
+    for k in range(1, n):
+        b[k] = b[k] - np.dot(a[k, 0:k], b[0:k])
+    b[n - 1] = b[n - 1] / a[n - 1, n - 1]
+    for k in range(n - 2, -1, -1):
+        b[k] = (b[k] - np.dot(a[k, k + 1:n], b[k + 1:n])) / a[k, k]
+    return b
