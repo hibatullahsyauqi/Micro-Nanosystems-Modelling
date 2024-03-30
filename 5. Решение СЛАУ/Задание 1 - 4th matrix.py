fbf6fecc16cc127466e32f97cb31f002a600b798
iqqqqquays <hibatullahsyauqiattaqi@gmail.com>
2024-03-30 11:51:03 +0300

    task 1.4

--- 5. Решение СЛАУ/Задание 1 - 4th matrix.py
+++ 5. Решение СЛАУ/Задание 1 - 4th matrix.py
@@ -1,17 +1,28 @@
-# Определение матрицы A
+def determinant(matrix):
+    n = len(matrix)
+    # случай для матрицы 2x2
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    # случай для матрицы nxn
+    else:
+        det = 0
+        for j in range(n):
+            det += (-1) ** j * matrix[0][j] * determinant(minor(matrix, 0, j))
+        return det
+
+def minor(matrix, row, col):
+    return [row[:col] + row[col+1:] for row in (matrix[:row] + matrix[row+1:])]
+
+# Матрица A
 A = [[4, 3, -1],
      [7, -2, 3],
      [5, -18, 13]]
 
-# Вычисление определителя для 3x3 матрицы
-def determinant(matrix):
-    return (matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
-            matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
-            matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]))
-
-# Проверка на линейную зависимость
+# Вычисляем определитель матрицы A
 det_A = determinant(A)
+
+# Проверяем, является ли определитель нулем
 if det_A == 0:
-    print("Матрицы линейно зависимы.")
+    print("Матрицы линейно-зависимы")
 else:
-    print("Матрицы линейно независимы.")
+    print("Матрицы линейно-независимы")
